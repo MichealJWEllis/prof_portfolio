b85288e3a59bd08f6550e79aef7127a7eaf07df6
mellis <mellis.eriecon@gmail.com>
2022-12-10 17:19:16 -0500

    Updates

---
+++ precache-manifest.cbda86ac0caff5c33bc0d04c85d9f2d8.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "17a934cfd63074fff9d4",
+    "url": "/prof_portfolio/static/css/main.cce91338.chunk.css"
+  },
+  {
+    "revision": "17a934cfd63074fff9d4",
+    "url": "/prof_portfolio/static/js/main.17a934cf.chunk.js"
+  },
+  {
+    "revision": "74bed0bbf3eb896f254c",
+    "url": "/prof_portfolio/static/css/1.adae5a82.chunk.css"
+  },
+  {
+    "revision": "74bed0bbf3eb896f254c",
+    "url": "/prof_portfolio/static/js/1.74bed0bb.chunk.js"
+  },
+  {
+    "revision": "e4a4de564b476c1b77e1",
+    "url": "/prof_portfolio/static/js/runtime~main.e4a4de56.js"
+  },
+  {
+    "revision": "02d0b59c34e899373891baa93b548ff7",
+    "url": "/prof_portfolio/static/media/cb8a0b256fcb0233020e58d48ced1bed.02d0b59c.jpg"
+  },
+  {
+    "revision": "25cf0d65cf2b2531723721f9163c8f76",
+    "url": "/prof_portfolio/static/media/3d207c1b1a65493bee0ffcc1223be658.25cf0d65.jpg"
+  },
+  {
+    "revision": "4ba5eefa3b13464ab382a8193b64f086",
+    "url": "/prof_portfolio/static/media/f161f2f803c5d37beb766c878b5ecc8c.4ba5eefa.jpg"
+  },
+  {
+    "revision": "238f81954a20fdfd0ad8e83a8a8bd34b",
+    "url": "/prof_portfolio/static/media/budget.238f8195.png"
+  },
+  {
+    "revision": "bf593eaa6463c20fbfe7b04cabc1127f",
+    "url": "/prof_portfolio/static/media/todo1.bf593eaa.png"
+  },
+  {
+    "revision": "ec0517b3935306eb2c5d7e51c01eb015",
+    "url": "/prof_portfolio/static/media/quiz.ec0517b3.png"
+  },
+  {
+    "revision": "1cf80d11493e9ad5b0f43bb40e2f6ed1",
+    "url": "/prof_portfolio/static/media/workday.1cf80d11.png"
+  },
+  {
+    "revision": "61c2bf4ee96e74506977001d16314c52",
+    "url": "/prof_portfolio/static/media/WeatherDash1.61c2bf4e.png"
+  },
+  {
+    "revision": "69822193bbd73f52a6b2986952591e05",
+    "url": "/prof_portfolio/static/media/foodfestival.69822193.png"
+  },
+  {
+    "revision": "4162ecb9e775848ab4121e80a4d390ae",
+    "url": "/prof_portfolio/static/media/rpsGame.4162ecb9.png"
+  },
+  {
+    "revision": "bf3a5f90129123047b2e307f37b3b5e5",
+    "url": "/prof_portfolio/static/media/Screenshot 2022-12-07 120420.bf3a5f90.png"
+  },
+  {
+    "revision": "4f7dc8af3f418c6e89402364a6006121",
+    "url": "/prof_portfolio/static/media/Screenshot 2022-12-07 120516.4f7dc8af.png"
+  },
+  {
+    "revision": "d6afa237ea3d4f85223277aee985b556",
+    "url": "/prof_portfolio/static/media/vid.d6afa237.mp4"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/prof_portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/prof_portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/prof_portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/prof_portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/prof_portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/prof_portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/prof_portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/prof_portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/prof_portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/prof_portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/prof_portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/prof_portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/prof_portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/prof_portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/prof_portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/prof_portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "418afb8f316e7bd1ccb8dff3163b6008",
+    "url": "/prof_portfolio/index.html"
+  }
+];
